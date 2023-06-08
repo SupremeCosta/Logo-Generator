3f6ad6891ed79b097a5c3137bc53523c090d4d25
Carlos Costa <carloscosta@MacBook-Pro.local>
2023-06-08 13:23:11 -0400

    added user answers to the index.js file

--- index.js
+++ index.js
@@ -27,3 +27,33 @@ rl.question('Enter up to three characters: ', (text) => {
     });
   });
 });
+
+function generateSVG(data) {
+    let shapeElement = '';
+  
+    switch (data.shape) {
+      case 'circle':
+        shapeElement = `<circle cx="150" cy="100" r="50" fill="${data.shapeColor}" />`;
+        break;
+      case 'triangle':
+        shapeElement = `<polygon points="150,50 100,150 200,150" fill="${data.shapeColor}" />`;
+        break;
+      case 'square':
+        shapeElement = `<rect x="100" y="50" width="100" height="100" fill="${data.shapeColor}" />`;
+        break;
+    }
+
+    const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      ${shapeElement}
+      <text x="150" y="100" fill="${data.textColor}" text-anchor="middle" alignment-baseline="middle" font-size="30">${data.text}</text>
+    </svg>`;
+  
+    fs.writeFile('logo.svg', svgContent, err => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log('Generated logo.svg');
+      }
+    });
+  }
